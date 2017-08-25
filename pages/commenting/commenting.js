fb432e5875c3bcdafad405820ee494be64d4f836
liyacheng <liyacheng@metadata.net.cn>
2017-08-25 18:34:25 +0800

    完善会议-待付款中左划出现删除、删除功能、统计金额功能，修改全选、单选

--- pages/commenting/commenting.js
+++ pages/commenting/commenting.js
@@ -10,19 +10,26 @@ Page({
         roldId: 2, //角色：1为专家，2为企业
         tabs: ['待确认', '已确认', '已结束'],
         tabs2: ['待付款', '已确认', '已结束'],
+        allItem: [{ value: '全选', checked: false }],
         activeIndex: 0,
         sliderOffset: 0,
         sliderLeft: 0,
         time: 'drop',
         money: 'litre',
-        accountMoney: 0,
-        allItem: [{ value: '全选', checked: false }],
         edit: false,
         editProfessor: false,
+        startX: '0',
+        delBtnWidth: '53',
+        accountMoney: 0,
+        demandMoney: 0,
+        professorMoney: 0,
+        totalNum: 0,
+        demandNum: 0,
+        professorNum: 0,
         list: [
             {
                 id: '1',
-                name: '姓名',
+                name: '姓名1',
                 college: '上海交通大学',
                 unit: '材料科学学院',
                 professionalTitle: '教授',
@@ -30,11 +37,12 @@ Page({
                 direction: 'ASIC/FOPI设计、测试，微电子行业研究',
                 field: '微电子、半导体、多媒体技术',
                 money: 10,
-                checked: false
+                checked: false,
+                txtStyle: ''
             },
             {
                 id: '2',
-                name: '姓名',
+                name: '姓名2',
                 college: '上海交通大学',
                 unit: '材料科学学院',
                 professionalTitle: '教授',
@@ -42,11 +50,12 @@ Page({
                 direction: 'ASIC/FOPI设计、测试，微电子行业研究',
                 field: '微电子、半导体、多媒体技术',
                 money: 10,
-                checked: false
+                checked: false,
+                txtStyle: ''
             },
             {
                 id: '3',
-                name: '姓名',
+                name: '姓名3',
                 college: '上海交通大学',
                 unit: '材料科学学院',
                 professionalTitle: '教授',
@@ -54,13 +63,14 @@ Page({
                 direction: 'ASIC/FOPI设计、测试，微电子行业研究',
                 field: '微电子、半导体、多媒体技术',
                 money: 10,
-                checked: false
+                checked: false,
+                txtStyle: ''
             }
         ],
         listProfessor: [
             {
                 id: '1',
-                name: '姓名',
+                name: '姓名4',
                 college: '上海交通大学',
                 unit: '材料科学学院',
                 professionalTitle: '教授',
@@ -72,7 +82,7 @@ Page({
             },
             {
                 id: '2',
-                name: '姓名',
+                name: '姓名5',
                 college: '上海交通大学',
                 unit: '材料科学学院',
                 professionalTitle: '教授',
@@ -84,7 +94,7 @@ Page({
             },
             {
                 id: '3',
-                name: '姓名',
+                name: '姓名6',
                 college: '上海交通大学',
                 unit: '材料科学学院',
                 professionalTitle: '教授',
@@ -194,8 +204,10 @@ Page({
     },
     // 全选
     checkAllChange: function (e) {
-        var checked = false;
+        var checked = false, demandNum = 0, professorNum = 0, demandMoney = 0, professorMoney = 0;
         var checkboxItems = this.data.allItem, values = e.detail.value;
+
+        // 修改全选状态
         for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
             checkboxItems[i].checked = false;
 
@@ -206,94 +218,296 @@ Page({
                     break;
                 }
             }
-
         };
-
+        // 根据全选状态修改列表状态
         var checkedList = this.data.list;
         for (var i = 0, lenI = checkedList.length; i < lenI; ++i) {
             checkedList[i].checked = checked;
+            demandMoney += checkedList[i].money
         }
-
         var checkedList2 = this.data.listProfessor;
         for (var i = 0, lenI = checkedList2.length; i < lenI; ++i) {
             checkedList2[i].checked = checked;
+            professorMoney += checkedList2[i].money
+        }
+        //设置选中项长度和金额
+        if (checked) {
+            demandNum = checkedList.length
+            professorNum = checkedList2.length
+        } else {
+            demandMoney = 0
+            professorMoney = 0
         }
 
+        // 更新列表状态
         this.setData({
             allItem: checkboxItems,
             list: checkedList,
             listProfessor: checkedList2,
+            demandNum: demandNum,
+            professorNum: professorNum,
+            demandMoney: demandMoney,
+            professorMoney: professorMoney,
+            accountMoney: demandMoney + professorMoney
         });
     },
-    // 需求沟通选择
+    // 多选
     checkboxChange: function (e) {
-        var checked = false;
-        var checkboxItems = this.data.list, values = e.detail.value;
-        for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-            checkboxItems[i].checked = false;
+        let that = this;
+        let list = [], money = 0, allItemNum = 0, totalNum = 0, itemNum = 0, allMoney = 0, itemMoney = 0, checked = false;
+        let listName = e.currentTarget.dataset.list, values = e.detail.value;
 
+        // 判断操作的列表
+        if (listName == 'demand') {
+            list = that.data.list;
+        } else {
+            list = that.data.listProfessor;
+        }
+
+        // 遍历修改状态
+        for (var i = 0, lenI = list.length; i < lenI; ++i) {
+            list[i].checked = false;
             for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-                if (checkboxItems[i].id == values[j]) {
-                    checkboxItems[i].checked = true;
+                if (list[i].id == values[j]) {
+                    list[i].checked = true;
+                    itemMoney += list[i].money;
                     break;
                 }
             }
         }
 
-        if (checkboxItems.length === values.length) {
-            checked = true
+        // 判断选中项的长度
+        itemNum = values.length
+        allItemNum = that.data.listProfessor.length + that.data.list.length
+
+        if (listName == 'demand') {
+            totalNum = itemNum + that.data.professorNum
+            allMoney = itemMoney + that.data.professorMoney
+        } else {
+            totalNum = itemNum + that.data.demandNum
+            allMoney = itemMoney + that.data.demandMoney
         }
 
+        // 修改全选状态
+        if (allItemNum === totalNum) {
+            checked = true
+        }
         var checkboxlist = this.data.allItem;
         for (var i = 0, lenI = checkboxlist.length; i < lenI; ++i) {
             checkboxlist[i].checked = checked;
         };
 
-        this.setData({
-            allItem: checkboxlist,
-            list: checkboxItems
-        });
-    },
-    // 专家约见选择
-    checkedProfessorChange: function (e) {
-        var checked = false;
-        var checkboxItems = this.data.listProfessor, values = e.detail.value;
-        for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-            checkboxItems[i].checked = false;
+        // 更新列表状态
+        if (listName == 'demand') {
+            this.setData({
+                allItem: checkboxlist,
+                list: list,
+                demandNum: itemNum,
+                accountMoney: allMoney,
+                demandMoney: itemMoney
+            })
+        } else {
+            this.setData({
+                allItem: checkboxlist,
+                listProfessor: list,
+                professorNum: itemNum,
+                accountMoney: allMoney,
+                professorMoney: itemMoney
+            })
+        }
 
-            for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-                if (checkboxItems[i].id == values[j]) {
-                    checkboxItems[i].checked = true;
-                    break;
+    },
+    // 编辑
+    editList: function (e) {
+        let list = [];
+        let that = this;
+        let listName = e.currentTarget.dataset.list
+        if (listName == 'demand') {
+            list = that.data.list;
+        } else {
+            list = that.data.listProfessor;
+        }
+        var delBtnWidth = this.data.delBtnWidth;
+        list.map(item => item.txtStyle = "left:-" + delBtnWidth + "px")
+
+        //更新列表的状态
+        if (listName == 'demand') {
+            list = that.data.list;
+            this.setData({
+                edit: true,
+                list: list
+            });
+        } else {
+            list = that.data.listProfessor;
+            this.setData({
+                editProfessor: true,
+                listProfessor: list
+            });
+        }
+
+    },
+    // 完成
+    submitList: function (e) {
+        let list = [];
+        let that = this;
+        let listName = e.currentTarget.dataset.list
+        if (listName == 'demand') {
+            list = that.data.list;
+        } else {
+            list = that.data.listProfessor;
+        }
+        list.map(item => item.txtStyle = "left:0")
+        //更新列表的状态
+        if (listName == 'demand') {
+            list = that.data.list;
+            this.setData({
+                edit: false,
+                list: list
+            });
+        } else {
+            list = that.data.listProfessor;
+            this.setData({
+                editProfessor: false,
+                listProfessor: list
+            });
+        }
+    },
+    //手指刚放到屏幕触发
+    touchS: function (e) {
+        // console.log("touchS", e);
+
+        if (e.touches.length == 1) {//判断是否只有一个触摸点
+            this.setData({
+
+                startX: e.touches[0].clientX//记录触摸起始位置的X坐标
+            });
+        }
+    },
+    //触摸时触发，手指在屏幕上每移动一次，触发一次
+    touchM: function (e) {
+        // console.log("touchM:", e);
+        var that = this
+        if (e.touches.length == 1) {
+
+            var moveX = e.touches[0].clientX;//记录触摸点位置的X坐标
+
+            var disX = that.data.startX - moveX;//计算手指起始点的X坐标与当前触摸点的X坐标的差值
+
+            var delBtnWidth = that.data.delBtnWidth;//delBtnWidth 为右侧按钮区域的宽度
+            var txtStyle = "";
+            if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+                txtStyle = "left:0px";
+            } else if (disX > 0) {//移动距离大于0，文本层left值等于手指移动距离
+                txtStyle = "left:-" + disX + "px";
+                if (disX >= delBtnWidth) {
+
+                    txtStyle = "left:-" + delBtnWidth + "px";//控制手指移动距离最大值为删除按钮的宽度
                 }
             }
+
+            var index = e.currentTarget.dataset.index;//获取手指触摸的是哪一个item
+
+            // 判断属于哪个列表
+            if (e.currentTarget.dataset.list == 'demand') {
+                var list = that.data.list;
+
+                list[index].txtStyle = txtStyle;//将拼接好的样式设置到当前item中
+                //更新列表的状态
+                this.setData({
+                    list: list
+                });
+            } else {
+                var list = that.data.listProfessor;
+
+                list[index].txtStyle = txtStyle;//将拼接好的样式设置到当前item中
+                //更新列表的状态
+                this.setData({
+                    listProfessor: list
+                });
+            }
         }
-
-        if (checkboxItems.length === values.length) {
-            checked = true
-        }
-
-        var checkboxlist = this.data.allItem;
-        for (var i = 0, lenI = checkboxlist.length; i < lenI; ++i) {
-            checkboxlist[i].checked = checked;
-        };
-
-        this.setData({
-            allItem: checkboxlist,
-            listProfessor: checkboxItems
-        });
     },
-    editDemand: function(){
-        this.setData({
-            edit: true
+    // 手指移动结束
+    touchE: function (e) {
+        // console.log("touchE", e);
+        var that = this
+        if (e.changedTouches.length == 1) {
+
+            var endX = e.changedTouches[0].clientX;//手指移动结束后触摸点位置的X坐标
+
+            var disX = that.data.startX - endX;//触摸开始与结束，手指移动的距离
+            var delBtnWidth = that.data.delBtnWidth;
+
+            var txtStyle = disX > delBtnWidth / 2 ? "left:-" + delBtnWidth + "px" : "left:0px";//如果距离小于删除按钮的1/2，不显示删除按钮
+
+
+            var index = e.currentTarget.dataset.index;//获取手指触摸的是哪一个item
+
+            // 判断属于哪个列表
+            if (e.currentTarget.dataset.list == 'demand') {
+                var list = that.data.list;
+
+                list[index].txtStyle = txtStyle;//将拼接好的样式设置到当前item中
+                //更新列表的状态
+                this.setData({
+                    list: list
+                });
+            } else {
+                var list = that.data.listProfessor;
+
+                list[index].txtStyle = txtStyle;//将拼接好的样式设置到当前item中
+                //更新列表的状态
+                this.setData({
+                    listProfessor: list
+                });
+            }
+        }
+    },
+    // 删除
+    delItem: function (e) {
+        let that = this;
+        let list = [];
+        let listName = e.currentTarget.dataset.list
+        let index = e.currentTarget.dataset.index; //获取删除项索引
+
+        if (listName == 'demand') {
+            list = that.data.list;
+        } else {
+            list = that.data.listProfessor;
+        }
+        wx.showModal({
+            title: '提示',
+            content: '确定删除该项内容？',
+            confirmText: "确定",
+            cancelText: "取消",
+            success: function (res) {
+                if (res.confirm) {
+
+                    list.splice(index, 1);
+
+                    //更新列表的状态
+                    if (listName == 'demand') {
+                        that.setData({
+                            list: list
+                        });
+                    } else {
+                        that.setData({
+                            listProfessor: list
+                        });
+                    }
+                } else {
+                    console.log('取消');
+                }
+            }
         });
+
     },
     /**
      * 生命周期函数--监听页面加载
      */
 
     onLoad: function () {
-        console.log('onLoad')
+        // console.log('onLoad')
         var that = this
         wx.getSystemInfo({
             success: function (res) {
